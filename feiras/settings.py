e6d51609348e8631a517b09161217a3bd23ea2b9
KaelCordeiro <kaellucaspinheirocordeiro@gmail.com>
2019-09-26 10:48:18 -0300

    'gestao_presenca'

--- feiras/settings.py
+++ feiras/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'certificados',
+    'gestao_presenca',
 ]
 
 MIDDLEWARE = [
@@ -113,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'BRT'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
