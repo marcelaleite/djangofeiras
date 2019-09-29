00dd28820aa6a51e545eb2edda1edaf764708952
Alexandre Bauer <alexandrebaua@hotmail.com>
2019-09-29 08:33:49 -0300

    fix url

--- feiras/urls.py
+++ feiras/urls.py
@@ -20,5 +20,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('certificados', include('certificados.urls'))
+    path('certificados/', include('certificados.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
