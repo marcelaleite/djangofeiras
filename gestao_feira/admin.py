8bc871b36097eed299fa4011ed52db897c8f32f4
Lipe <lipe_tenfen@hotmail.com>
2019-10-29 10:42:40 -0300

    add admin e apps de gestão feira

---
+++ gestao_feira/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+from .models import Feira
+from .models import Cronograma
+from .models import Organizadores
+from .models import Patrocinadores
+from .models import Observacoes
+from .models import Instituicao
+
+admin.site.register(Feira)
+admin.site.register(Cronograma)
+admin.site.register(Organizadores)
+admin.site.register(Patrocinadores)
+admin.site.register(Observacoes)
+admin.site.register(Instituicao)
---
+++ gestao_feira/apps.py
@@ -0,0 +1,4 @@
+from django.apps import AppConfig
+
+class GestaoFeiraConfig(AppConfig):
+    name = 'gestao_feira'
