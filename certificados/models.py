7abd0acffbf71316adc77dcae5bb481636b219ec
Alexandre Bauer <alexandrebaua@hotmail.com>
2019-11-23 11:31:46 -0300

    Adicionado campo categoria no modelo de certificado

--- certificados/models.py
+++ certificados/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib import auth
 from django.urls import reverse
 
+from gestao_feira.models import Categoria
+
 # Create your models here.
 
 class Certificado(models.Model):
@@ -9,6 +11,7 @@ class Certificado(models.Model):
     ptext = models.CharField(max_length=500, null=False, blank=False)
     imagem_fundo = models.ImageField(upload_to="certificados/fundo/", null=True, blank=True)
     imagem_evento = models.ImageField(upload_to="certificados/evento/", null=True, blank=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return self.nome_modelo
